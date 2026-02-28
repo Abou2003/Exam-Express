e1526562defc060442ffeb9c2170d5efab1c8a64
hp <sowaboubacry54@gmail.com>
2026-02-28 17:46:26 +0000

    Correction bug affichage tâches1

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
     host: 'localhost',
     user : 'root',
-    password: 'ngarane2310',
+    password: 'XXXXXXXXXX',
     database: 'projet_exam'
 });
 
